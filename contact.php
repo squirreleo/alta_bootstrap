28d6ab14289387a63984a4a528127036860450ac
Angela Downs <angeladowns@yahoo.com>
2014-03-26 11:41:33 -0400

    Updated javascript and php validation in contact form

--- contact.php
+++ contact.php
@@ -1,38 +1,31 @@
 <?php
- include_once("wsp_captcha.php");
 
-  if (WSP_CheckImageCode() != "OK") {
-    die("The image code you have entered is incorrect. Please, click the 'Back' button of your browser and type the correct one.");
-  }
-  
-  $to = "alynch1224@yahoo.com"; 
-  $from = $_REQUEST['inputEmail'] ; 
-  $name = $_REQUEST['inputName'] ; 
-  $headers = "From: $name"; 
-  $subject = "Web Contact Form Data"; 
- 
-  $fields = array(); 
-  $fields{"inputName"} = "Name"; 
-  $fields{"inputCompany"} = "Company"; 
-  $fields{"inputEmail"} = "Email"; 
-  $fields{"inputPhone"} = "Phone"; 
-  $fields{"inputNotes"} = "Message"; 
- 
-  $body = "The following information was submitted via the Contact form on Alta's Web site:\n\n"; foreach($fields as $a => $b){ 	$body .= sprintf("%20s: %s\n",$b,$_REQUEST[$a]); } 
- 
-  if($from === '') {header('Location: http://www.angeladowns.com/alta/error.html');} 
-  else { 
-  if($name === '') {header('Location: http://www.angeladowns.com/alta/error.html');} 
-  else { 
-  $send = mail($to, $subject, $body, $headers); 
-  
-  if($send) 
-  {
-      header('Location: http://www.angeladowns.com/alta/thankyou.html'); exit;
-  } 
-  else 
-  {
-      header('Location: http://www.angeladowns.com/alta/error.html'); exit;
-  }
-  }
- }
+// check if fields passed are empty
+if(empty($_POST['name'])  		||
+   empty($_POST['email']) 		||
+   empty($_POST['message'])	||
+   ($_POST['notcaptcha'] !== "on") ||
+   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
+   {
+	echo "No arguments Provided!";
+	return false;
+   }
+
+$name = $_POST['name'];
+$company = $_POST['company'];
+$email_address = $_POST['email'];
+$phone = $_POST['phone'];
+$message = $_POST['message'];
+
+// create email body and send it	
+$to = 'alynch1224@yahoo.com'; 
+$email_subject = "Contact form submitted by:  $name";
+$email_body = "You have received a new message via the contact form on Alta's Web site. \n\n".
+				  "Name: $name \n ".
+                                  "Company: $company \n".
+				  "Email: $email_address\n".
+                                  "Phone: $phone\n".
+                                  "Message: $message";
+$headers = "From: sales@altainc.com\n";  // change to sales@altainc.com to avoid junk folder?
+$headers .= "Reply-To: $email_address";	
+mail($to,$email_subject,$email_body,$headers);
